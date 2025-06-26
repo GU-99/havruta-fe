e857c37233fd5a97f59244ddf67cd2c26d5d25d9
Seok93 <gkdltjr@naver.com>
2025-06-26 21:55:50 +0900

    Config: #1 MSW 모킹을 Node 환경으로 통합하고, Instrumentation으로 활성화

--- src/config/msw/MockProvider.tsx
+++ src/config/msw/MockProvider.tsx
@@ -6,6 +6,7 @@ type MockProviderProps = {
   children: React.ReactNode;
 }
 
+//* Info: Node 환경으로 통합하여 더이상 사용하지 않지만, 이후 활용이나 참고를 위해 남깁니다.
 export default function MockProvider({ children }: MockProviderProps) {
   const [isMockLoading, setIsMockLoading] = useState(true);
 
