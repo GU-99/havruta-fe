81ce172fe289d1203ff29d8d81163f553c5afa59
yoonyesol <maniac970430@naver.com>
2025-09-21 20:51:34 +0900

    UI: #8 스타일링 디테일 수정

--- src/app/mypage/components/ProfileCard.tsx
+++ src/app/mypage/components/ProfileCard.tsx
@@ -12,12 +12,12 @@ export const ProfileCard = () => {
     const handleChangePassword = () => alert("비밀번호 변경");
 
     return (
-        <div className="flex flex-col max-w-[1280px] mx-auto gap-[10px]">
-            <div className="flex flex-row h-[54px] font-bold p-[12px] gap-[20px] bg-[#F7F7FA] 
-                rounded-[20px] border-1 border-[#E1E1E4] text-[15px] items-center">
-                <div className="bg-white rounded-md border-1 border-[#E1E1E4]">
+        <div className="flex flex-col max-w-[1280px] mx-auto gap-2.5">
+            {/* 헤더 */}
+            <div className="flex flex-row font-bold p-4 gap-3 bg-[#F7F7FA] rounded-[20px] border border-[#E1E1E4] text-[15px] items-center">
+                <div className="bg-white rounded-md p-2 border border-[#E1E1E4]">
                     <Image
-                        src='/images/profile-icon.png'
+                        src="/images/profile-icon.png"
                         alt="프로필 사진"
                         width={24}
                         height={24}
@@ -27,10 +27,9 @@ export const ProfileCard = () => {
                 나의 프로필
             </div>
 
-            <div className="flex flex-col p-[12px] gap-[20px] bg-[#F7F7FA] rounded-[20px] border-1 border-[#E1E1E4]">
-                {/* 프로필 사진 + 버튼 */}
-                <div className="flex flex-row h-[220px] gap-[20px] bg-white
-                rounded-[20px] text-[15px] items-center">
+            {/* 프로필 사진 + 정보 수정 */}
+            <div className="flex flex-col p-4 gap-5 bg-[#F7F7FA] rounded-[20px] border border-[#E1E1E4]">
+                <div className="flex flex-row h-[220px] gap-5 md:bg-white rounded-[20px] text-[15px] items-center">
                     <div className="size-[220px] relative overflow-hidden rounded-[20px] mx-auto md:mx-0">
                         <Image
                             src={profileImg}
@@ -40,23 +39,23 @@ export const ProfileCard = () => {
                         />
                     </div>
 
-                    <div className="hidden md:flex flex-col gap-[10px]">
-                        <div className="flex flex-row gap-[10px]">
+                    <div className="hidden md:flex flex-col gap-2.5">
+                        <div className="flex flex-row gap-2.5">
                             <button
-                                className="bg-[#F5F5F5] rounded-[20px] px-[22px] py-[8px]"
+                                className="bg-[#F5F5F5] rounded-[12px] px-[14px] py-[7px] text-[13px] cursor-pointer"
                                 onClick={handleChangePhoto}
                             >
                                 프로필 사진 변경하기
                             </button>
                             <button
-                                className="bg-[#F5F5F5] rounded-[20px] px-[22px] py-[8px]"
+                                className="bg-[#F5F5F5] rounded-[12px] px-[14px] py-[7px] text-[13px] cursor-pointer"
                                 onClick={handleResetPhoto}
                             >
                                 프로필 초기화
                             </button>
                         </div>
 
-                        <div className="text-[14px]">
+                        <div className="text-[11px]">
                             <p>최소 250x250 px 권장</p>
                             <p>JPG, JPEG, PNG</p>
                         </div>
@@ -64,21 +63,22 @@ export const ProfileCard = () => {
                 </div>
 
                 {/* 기본 정보 */}
-                <div className="flex flex-col h-[196px] px-[30px] py-[20px] gap-[20px] bg-white
-                rounded-[20px] text-[15px] justify-center"
-
-                >
-                    <div className="flex flex-row justify-between items-center" >
-                        <h3 className="text-[20px] font-bold">기본 정보</h3>
-                        <button className="bg-[#F5F5F5] rounded-[16px] px-[10px] py-[4px] text-[16px]"
-                            onClick={handleEditProfile}>수정</button>
+                <div className="flex flex-col px-6 py-4 md:px-[60px] md:py-10 gap-5 bg-white rounded-[20px] justify-center">
+                    <div className="flex flex-row justify-between items-center">
+                        <h3 className="text-base md:text-xl font-medium">기본 정보</h3>
+                        <button
+                            className="bg-[#F5F5F5] rounded-[12px] px-[14px] py-[7px] text-[13px] cursor-pointer"
+                            onClick={handleEditProfile}
+                        >
+                            수정
+                        </button>
                     </div>
-                    <div className="flex flex-col md:flex-row gap-[20px] md:gap-[30px]">
-                        <div className="flex flex-col gap-[6px]">
+                    <div className="flex flex-col md:flex-row gap-5 md:gap-[60px] text-[14px] md:text-[15px]">
+                        <div className="flex flex-col gap-1.5">
                             <p className="text-[#A1A1A4] text-sm">이메일</p>
                             <p>havruta@naver.com</p>
                         </div>
-                        <div className="flex flex-col gap-[6px]">
+                        <div className="flex flex-col gap-1.5">
                             <p className="text-[#A1A1A4] text-sm">닉네임</p>
                             <p>하브루타</p>
                         </div>
@@ -86,21 +86,18 @@ export const ProfileCard = () => {
                 </div>
 
                 {/* 비밀번호 수정 */}
-                <div className="flex flex-col h-[122px] px-[30px] py-[20px] gap-[20px] bg-white
-                rounded-[20px] text-[15px] justify-center"
-
-                >
-                    <div className="flex flex-row justify-between items-center" >
-                        <h3 className="text-[20px] font-bold">비밀번호 수정</h3>
-                        <button className="bg-[#F5F5F5] rounded-[16px] px-[10px] py-[4px] text-[16px]"
-                            onClick={handleChangePassword}>수정</button>
+                <div className="flex flex-col px-6 py-4 md:px-[60px] md:py-10 gap-5 bg-white rounded-[20px] text-[15px] justify-center">
+                    <div className="flex flex-row justify-between items-center">
+                        <h3 className="text-base md:text-xl font-medium">비밀번호 수정</h3>
+                        <button
+                            className="bg-[#F5F5F5] rounded-[12px] px-[14px] py-[7px] text-[13px] cursor-pointer"
+                            onClick={handleChangePassword}
+                        >
+                            수정
+                        </button>
                     </div>
-
                 </div>
-
             </div>
-
-
         </div>
     );
 };
